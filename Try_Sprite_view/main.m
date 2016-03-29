81eb3d8a094875ee5b1e50e6d55521b1c05ef2ba
irons163 <irons@ironsdeMacBook-Air.local>
2016-03-29 21:57:18 +0800

    Initial Commit

---
+++ Try_Sprite_view/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Try_Sprite_view
+//
+//  Created by irons on 2016/3/29.
+//  Copyright (c) 2016年 irons. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ Try_Sprite_viewTests/Try_Sprite_viewTests.m
@@ -0,0 +1,40 @@
+//
+//  Try_Sprite_viewTests.m
+//  Try_Sprite_viewTests
+//
+//  Created by irons on 2016/3/29.
+//  Copyright (c) 2016年 irons. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface Try_Sprite_viewTests : XCTestCase
+
+@end
+
+@implementation Try_Sprite_viewTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
